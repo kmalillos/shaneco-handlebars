e9e5c622ccbf44f72373f32c3f6b83b0043691dd
Kaylah Malillos <malillosk@gmail.com>
2019-11-20 00:10:13 -0700

    styled page template

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
         res.render("index");
     });
 
-    app.get("/menu", function (req, res){
+    app.get("/:menu", function (req, res){
         res.render("page");
     })
 };
