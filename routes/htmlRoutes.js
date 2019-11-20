d28dc7d0b732d4e88ac0d4a4a54ff7dc43b7053f
Kaylah Malillos <malillosk@gmail.com>
2019-11-20 00:21:20 -0700

    added page templates

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,7 +5,27 @@ module.exports = function(app) {
         res.render("index");
     });
 
-    app.get("/:menu", function (req, res){
-        res.render("page");
+    app.get("/engagement", function (req, res){
+        res.render("page", { title: "Engagement", shop: "Engagement Rings"});
+    })
+
+    app.get("/wedding", function (req, res){
+        res.render("page", { title: "Wedding", shop: "Wedding Rings and Bands" });
+    })
+
+    app.get("/diamondsandgems", function (req, res){
+        res.render("page", { title: "Diamonds and Gems", shop: "Loose Stones, Diamonds and Gems"});
+    })
+
+    app.get("/Jewelry", function (req, res){
+        res.render("page", { title: "Jewelry", shop: "Rings, Bracelets, Necklaces, and More"});
+    })
+
+    app.get("/personalize", function (req, res){
+        res.render("page", { title: "Personalize", shop: "Personalized Gifts"});
+    })
+
+    app.get("/about", function (req, res){
+        res.render("page", { title: "About", shop: "Information"});
     })
 };
